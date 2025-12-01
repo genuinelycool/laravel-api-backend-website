69c50c5616ee91acc590fe5315480be7ab7d3235
genuinelycool <genuinelycool@gmail.com>
2025-12-02 00:07:56 +0545

    28.1 -Two-Factor Authentication Part 1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\VerificationCodeMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -16,5 +19,26 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/login');
+    }
+
+    public function AdminLogin(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            $verificationCode = random_int(100000, 999999);
+
+            session(['verification_code' => $verificationCode, 'user_id' => $user->id]);
+
+            Mail::to($user->email)->send(new VerificationCodeMail($verificationCode));
+
+            Auth::logout();
+
+            return redirect()->route('custom.verification.form')->with('status', 'Verification code sent to your email');
+        }
+
+        return redirect()->back()->withErrors(['email' => 'Invalid Credentials provided']);
     }
 }
