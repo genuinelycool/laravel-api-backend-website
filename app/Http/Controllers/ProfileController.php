8c6880f878ed8cbca9deaa878efa776a1e54bfcf
genuinelycool <genuinelycool@gmail.com>
2025-12-02 16:01:49 +0545

    11 -Adding Toster In For View Message

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -41,7 +41,13 @@ class ProfileController extends Controller
         }
 
         $data->save();
-        return redirect()->back();
+
+        $notification = array(
+            'message' => 'Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
     }
 
     private function deleteOldImage(string $oldPhotoPath): void
