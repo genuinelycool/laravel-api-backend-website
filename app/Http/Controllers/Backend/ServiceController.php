ef144ed30722465ebae4ebcd670db77c9264f068
genuinelycool <genuinelycool@gmail.com>
2025-12-03 20:32:34 +0545

    30.1 -Admin Backend Services Option Setup Part 1

---
+++ app/Http/Controllers/Backend/ServiceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Service;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Imagick\Driver;
+
+class ServiceController extends Controller
+{
+    public function AllService()
+    {
+        $service = Service::latest()->get();
+        return view('backend.service.all_service', compact('service'));
+    }
+}
