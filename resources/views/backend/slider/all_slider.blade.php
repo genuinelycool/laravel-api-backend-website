ba2936d2e71ab1496d5ca402573dba240eff7c49
genuinelycool <genuinelycool@gmail.com>
2025-12-03 20:04:28 +0545

    9 -Admin Backend Slider Option Setup Part 8

--- resources/views/backend/slider/all_slider.blade.php
+++ resources/views/backend/slider/all_slider.blade.php
@@ -46,7 +46,8 @@
                                             <td>
                                                 <a href="{{ route('edit.slider', $item->id) }}"
                                                     class="btn btn-success btn-sm">Edit</a>
-                                                <a href="" class="btn btn-danger btn-sm" id="delete">Delete</a>
+                                                <a href="{{ route('delete.slider', $item->id) }}"
+                                                    class="btn btn-danger btn-sm" id="delete">Delete</a>
                                             </td>
                                         </tr>
                                     @endforeach
