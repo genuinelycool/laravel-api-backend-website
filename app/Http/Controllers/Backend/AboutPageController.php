90fc317ed9137dd8976db3538a63ccfd75cfef7d
genuinelycool <genuinelycool@gmail.com>
2025-12-05 19:46:29 +0545

    2 -About Us Page Setup Part 2

--- app/Http/Controllers/Backend/AboutPageController.php
+++ app/Http/Controllers/Backend/AboutPageController.php
@@ -16,4 +16,59 @@ class AboutPageController extends Controller
 
         return view('backend.about.about_us', compact('about'));
     }
+
+    public function UpdateAboutPage(Request $request)
+    {
+        $about_id = $request->id;
+        $about = AboutPage::find($about_id);
+
+        if ($request->file('image')) {
+            $image = $request->file('image');
+
+            $manager = new ImageManager(new Driver());
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img->resize(960, 679)->save(public_path('upload/about/' . $name_gen));
+            $save_url = 'upload/about/' . $name_gen;
+
+            if (file_exists(public_path($about->image))) {
+                unlink(public_path($about->image));
+            }
+
+            $about->update([
+                'title' => $request->title,
+                'phone' => $request->phone,
+                'setup_growth' => $request->setup_growth,
+                'problem_solving' => $request->problem_solving,
+                'passive_income' => $request->passive_income,
+                'goal_achiever' => $request->goal_achiever,
+                'description' => $request->description,
+                'image' => $save_url,
+            ]);
+
+            $notification = array(
+                'message' => 'Updated with Image Successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->back()->with($notification);
+        } else {
+            $about->update([
+                'title' => $request->title,
+                'phone' => $request->phone,
+                'setup_growth' => $request->setup_growth,
+                'problem_solving' => $request->problem_solving,
+                'passive_income' => $request->passive_income,
+                'goal_achiever' => $request->goal_achiever,
+                'description' => $request->description,
+            ]);
+
+            $notification = array(
+                'message' => 'Updated without Image Successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->back()->with($notification);
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -93,6 +93,6 @@ Route::middleware('auth')->group(function () {
 
     Route::controller(AboutPageController::class)->group(function () {
         Route::get('/about/page', 'AboutPage')->name('about.page');
-        Route::post('/update/site/setting', 'UpdateSiteSetting')->name('update.site.setting');
+        Route::post('/update/aboutus', 'UpdateAboutPage')->name('update.aboutus');
     });
 });
