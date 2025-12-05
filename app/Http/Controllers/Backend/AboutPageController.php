2cea2845e6fae65cfaf7dac527c1844be8fb91f2
genuinelycool <genuinelycool@gmail.com>
2025-12-05 19:30:56 +0545

    37.1 -About Us Page Setup Part 1

---
+++ app/Http/Controllers/Backend/AboutPageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Imagick\Driver;
+use App\Models\AboutPage;
+
+class AboutPageController extends Controller
+{
+    public function AboutPage()
+    {
+        $about = AboutPage::find(1);
+
+        return view('backend.about.about_us', compact('about'));
+    }
+}
